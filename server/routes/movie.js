0367fed0e2ffe867a19881c381a8e1903620ed05
dandy <1055671428@qq.com>
2019-07-15 20:53:13 +0800

    拆分服务层和路由层,向外暴露api

--- server/routes/movie.js
+++ server/routes/movie.js
@@ -1,39 +1,34 @@
+// 路由层 --- 接受请求
 // 电影相关的接口入口
 const mongoose = require('mongoose')
 const { controller, get} = require('../lib/decorators.js')
-
+const {gettAllMovies, getMovieDetail, getRelativeMovies} = require('../service/movie.js')
 // 利用装饰器来拆分处理路由
 @controller('api/v0/movies')
 export class MovieController {
     @get('/')
     async getMovies(ctx, next) { // 获取全部电影
-        // 获取电影模型
-        const Movie = mongoose.model('Movie')
-        // 查询全部电影数据并按创建的顺序倒序排列
-        const movies = await Movie.find({}).sort({
-            'meta.createdAt': -1
-        })
-
+        const {type, year} = ctx.query
+        const movies = await gettAllMovies(type,year)
         ctx.body = {
             movies
         }
-        next()
+        // next()
     }
 
     @get('/:id') 
     async getMovieDetail(ctx, next) { // 获取单个电影的详情
-        // 获取电影模型
-        const Movie = mongoose.model('Movie')
         // 拿到参数id
         const id = ctx.params.id
-        // 查询全部电影数据并按创建的顺序倒序排列
-        const movie = await Movie.findOne({
-            _id: id
-        })
-
+        const movie = await getMovieDetail(id)
+        const relativeMovies = await getRelativeMovies(movie)
         ctx.body = {
-            movie
+            data: {
+                movie,
+                relativeMovies
+            },
+            success: true
         }
-        next()
+        // next()
     }
 }
---
+++ server/service/movie.js
@@ -0,0 +1,37 @@
+// 服务层 --- 查询数据(与数据库交互)
+
+const mongoose = require('mongoose')
+const Movie = mongoose.model('Movie')
+
+
+// 获得全部电影
+export const gettAllMovies = async (type, year) => {
+  let query = {}
+  if (type) {
+    query.movieTypes = {
+      $in: [type]
+    }
+  }
+  if (year) {
+    query.year = year
+  }
+  const movies = await Movie.find(query)
+  return movies
+}
+
+// 获得电影详情
+export const getMovieDetail = async (id) => {
+  const movie = await Movie.findOne({ _id: id })
+  return movie
+}
+
+// 获取相关电影
+export const getRelativeMovies = async (movie) => {
+  const movies = await Movie.find({
+    movieTypes: {
+      $in: movie.movieTypes
+    }
+  })
+  return movies
+}
+
