44e6dc8f9bfc77cbe23c4e3c3cdbc917673f8ff3
dandy <1055671428@qq.com>
2019-07-16 09:51:29 +0800

    parcel实现sass打包

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// postcss配置
+module.exports = {
+    plugins: [
+      require('autoprefixer'), // 自动加前缀
+      require('postcss-nested') // css 未来语法
+    ]
+  }
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './assets/common.sass'
+$('#app').html('web-percel')
+$('#app').click(function(){
+    console.log('点击事件')
+})
